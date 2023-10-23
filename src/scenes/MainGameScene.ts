e943f63bcd8eb10138c69bab637f80bbc25b2f18
Artem Panasiuk <artem.panasiuk@sigma.software>
2023-10-23 13:09:14 +0300

    Delete box

--- src/scenes/MainGameScene.ts
+++ src/scenes/MainGameScene.ts
@@ -2,11 +2,9 @@ import { Experience } from '../engine/Experience'
 import { Engine } from '../engine/Engine'
 import * as THREE from 'three'
 import { Resource } from '../engine/Resources'
-import { Box } from '../demo/Box'
-import { AnimationMixer, Object3D, Vector3 } from 'three'
+import { AnimationMixer, Object3D } from 'three'
 import { toogleHelpers } from '../signals/signals'
 import { Obstacle } from '../controls/Obstacle'
-import * as CANNON from 'cannon-es'
 
 export class MainGameScene implements Experience {
   resources: Resource[] = [
@@ -21,10 +19,7 @@ export class MainGameScene implements Experience {
       path: '../../assets/Brain.glb',
     },
   ]
-  private unit: Box = new Box()
   private obstacle: Obstacle = new Obstacle()
-  private animationStep = 0.5
-  private unitVector: Vector3 = new Vector3()
   private gridHelper: Object3D = new THREE.GridHelper(100, 50)
   private lightHelper!: Object3D
   private axes: Object3D = new THREE.AxesHelper(100)
@@ -51,21 +46,9 @@ export class MainGameScene implements Experience {
     directionalLight.castShadow = true
     directionalLight.position.set(2, 2, 2)
     this.obstacle.position.set(0, 1, 5)
-    let cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
-    let cubeBody = new CANNON.Body({ mass: 1, shape: new CANNON.Sphere(10) })
-    cubeBody.addShape(cubeShape)
-    cubeBody.position.set(0, 1, 5)
     this.lightHelper = new THREE.DirectionalLightHelper(directionalLight)
     this.engine.scene.add(directionalLight, this.gridHelper, this.obstacle)
-    this.unit.add(this.lightHelper, this.axes)
     this.hideHelpers()
-    this.unit.castShadow = true
-    this.unit.rotation.y = Math.PI
-    this.unit.position.set(0, 0.5, 20)
-    this.engine.scene.add(this.unit)
-    window.addEventListener('keydown', (e) => {
-      this.onChangePosition(e.key)
-    })
 
     this.brainMan = this.engine.resources.getItem('BrainMan')
     this.brainMan.scene.position.set(2, 0, 20)
@@ -95,26 +78,8 @@ export class MainGameScene implements Experience {
 
   update(delta: number) {
     this.mixer.update(delta)
-    this.unit.position.z += this.unitVector.z
-    this.unit.position.x += this.unitVector.x
-    this.unitVector.multiplyScalar(0.8)
     this.updateCamera()
   }
 
   updateCamera() {}
-
-  onChangePosition(key: string) {
-    if (key === 'w') {
-      this.unitVector.z -= this.animationStep
-    }
-    if (key === 's') {
-      this.unitVector.z += this.animationStep
-    }
-    if (key === 'd') {
-      this.unitVector.x += this.animationStep
-    }
-    if (key === 'a') {
-      this.unitVector.x -= this.animationStep
-    }
-  }
 }
